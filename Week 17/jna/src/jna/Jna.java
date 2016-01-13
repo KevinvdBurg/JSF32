12fde48d8c54e3f9d4baefe794ee78c354ff63e1
MartinDrost <info@martindrost.nl>
2016-01-13 13:54:13 +0100

    Week 17 initial commit

---
+++ Week 17/jna/src/jna/Jna.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jna;
+import com.sun.jna.*;
+
+/**
+ *
+ * @author Martin Drost <info@martindrost.nl>
+ */
+public class Jna {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
